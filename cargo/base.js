d31a827bddb0fedb350c4c0d399c8902291982e4
Joe Tessler <joe.r.tessler@gmail.com>
2013-02-05 00:51:23 -0600

    Converted hello world to canvas graphics.

--- cargo/base.js
+++ cargo/base.js
@@ -6,14 +6,19 @@
 
 goog.provide('cargo.base');
 
-goog.require('goog.dom');
+goog.require('goog.graphics');
 
 
 /**
  * Sets up the UI and initializes all game code.
  */
 cargo.base.main = function() {
-  var node = goog.dom.createTextNode('Hello, World!');
-  goog.dom.appendChild(goog.dom.getDocument().body, node);
+  var canvas = new goog.graphics.CanvasGraphics(500, 500);
+  var font = new goog.graphics.Font(40, 'Arial');
+  var stroke = new goog.graphics.Stroke(1, 'black');
+  var fill = new goog.graphics.SolidFill('black');
+  canvas.render();
+  canvas.drawText(
+      'Hello, World!', 200, 240, 100, 20, 'left', 'top', font, stroke, fill);
 };
 goog.exportSymbol('main', cargo.base.main);
