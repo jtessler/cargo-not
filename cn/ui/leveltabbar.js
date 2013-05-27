e795885a7d6d962a2a44f9c1607a05365412e79e
Joe Tessler <joe.r.tessler@gmail.com>
2013-05-27 03:46:48 -0500

    Added difficulty background images and select visuals.

--- cn/ui/leveltabbar.js
+++ cn/ui/leveltabbar.js
@@ -8,7 +8,8 @@ goog.provide('cn.ui.LevelTabBar');
 
 goog.require('cn.LevelData.levelpacks');
 goog.require('cn.model.Game');
-goog.require('goog.object');
+goog.require('goog.array');
+goog.require('goog.string');
 goog.require('goog.ui.Tab');
 goog.require('goog.ui.TabBar');
 
@@ -26,9 +27,23 @@ cn.ui.LevelTabBar = function(game, ui, opt_domHelper) {
   this.game_ = game;
   this.ui_ = ui;
 
-  goog.object.forEach(cn.LevelData.levelpacks, function(levels, name) {
-    this.addChild(new goog.ui.Tab(name), true);
+  // TODO(joseph): Handle this better.
+  var levelpacks = [
+    goog.getCssName('goog-tab-tutorial'),
+    goog.getCssName('goog-tab-easy'),
+    goog.getCssName('goog-tab-medium'),
+    goog.getCssName('goog-tab-hard'),
+    goog.getCssName('goog-tab-crazy'),
+    goog.getCssName('goog-tab-impossible')
+  ];
+  goog.array.forEach(levelpacks, function(name) {
+    var tab = new goog.ui.Tab(null);
+    tab.setTooltip(goog.string.toTitleCase(name));
+    tab.addClassName(name);
+    this.addChild(tab, true);
   }, this);
+
+  this.setSelectedTabIndex(0);
 };
 goog.inherits(cn.ui.LevelTabBar, goog.ui.TabBar);
 
